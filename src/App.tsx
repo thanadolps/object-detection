6a240437ebd02515cfb0d9a9d5bc2f0fc1c73efb
Thanadol Chitthamlerd <thanadolps@gmail.com>
2022-01-22 19:02:21 +0700

    Upload support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Accordion, AccordionDetails, AccordionSummary, AppBar, Button, Container, Slider, Stack, Switch, Toolbar, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, AppBar, Box, Button, Container, FormControlLabel, Input, Slider, Stack, Switch, Toolbar, Typography } from '@mui/material';
 import Webcam from 'react-webcam';
 import cv, { Mat, Rect } from "opencv-ts";
+import { CovarFlags } from 'opencv-ts/src/core/Core';
 
 
 
@@ -15,16 +16,14 @@ function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function pollRef<T>(ref: React.RefObject<T>) {
-  for (let i = 0; i < 100; i++) {
-    if (ref.current !== null) {
-      return ref.current;
-    }
-    await sleep(100);
-  }
-  return null;
+function readBase64(file: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onloadend = () => resolve(reader.result as string)
+    reader.onerror = reject
+    reader.readAsDataURL(file)
+  })
 }
-
 
 function App() {
   const webcamRef = useRef<Webcam>(null);
@@ -46,14 +45,26 @@ function App() {
     setDisplayImage(img);
   }
 
+  const handleUpload = async (file: File) => {
+    const img = await readBase64(file);
+    setDisplayImage(img);
+  }
+
   return (
     <Stack direction="column" spacing={2}>
-      <AppBar position='static'>
+      <AppBar position='sticky'>
         <Toolbar>
-          <Typography variant='h6'>Cloth scan</Typography>
+          <Typography variant='h6' sx={{ flexGrow: 1 }}>Cloth scan</Typography>
+
+          <FormControlLabel
+            control={<Switch color='secondary' checked={webcamEnable} onChange={() => setWebcamEnable(!webcamEnable)} />}
+            label="Camera"
+            sx={{ float: 'right' }}
+          />
         </Toolbar>
       </AppBar>
-      <Switch checked={webcamEnable} onChange={() => setWebcamEnable(!webcamEnable)} />
+
+
 
       {webcamEnable &&
         < Webcam
@@ -72,9 +83,41 @@ function App() {
         </>
       )}
 
-      <Button onClick={capture}>capture</Button>
-    </Stack>
+      <Button variant="contained" onClick={capture}>capture</Button>
+      <label>
+        <input accept="image/*" type="file" hidden onChange={(ev) => handleUpload(ev.currentTarget.files![0])} />
+        <Button variant="contained" component="span">Upload</Button>
+      </label>
+    </Stack >
   );
+}
+
+function processImage(image: Mat, options: { threshould: number }) {
+  const gray = new cv.Mat();
+  cv.cvtColor(image, gray, cv.COLOR_BGR2GRAY);
+
+  const thres = new cv.Mat();
+  cv.threshold(gray, thres, options.threshould, 255, cv.THRESH_BINARY);
+
+  const cons = new cv.MatVector();
+  const hir = new cv.Mat();
+  cv.findContours(thres, cons, hir, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+  for (let i = 0; i < cons.size(); i++) {
+    const c = cons.get(i);
+    const rect = cv.boundingRect(c);
+    cv.rectangle(
+      image,
+      new cv.Point(rect.x, rect.y),
+      new cv.Point(rect.x + rect.width, rect.y + rect.height),
+      new cv.Scalar(255, 255, 0, 255),
+      2
+    )
+  }
+
+  return {
+    gray, thres, cons, detected: image
+  }
 }
 
 
@@ -88,44 +131,23 @@ function CV(props: { img: string }) {
   const [objCount, setObjCount] = useState<number | null>(null);
 
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const task = async () => {
       await sleep(0);
 
       const image = cv.imread(imageRef.current!);
 
-      const gray = new cv.Mat();
-      cv.cvtColor(image, gray, cv.COLOR_BGR2GRAY);
+      const { gray, detected, thres, cons } = processImage(image, { threshould: threshould });
+
       cv.imshow(canvasGrayRef.current!, gray);
-
-      const thres = new cv.Mat();
-      cv.threshold(gray, thres, threshould, 255, cv.THRESH_BINARY);
       cv.imshow(canvasThresRef.current!, thres);
-
-      const cons = new cv.MatVector();
-      const hir = new cv.Mat();
-      cv.findContours(thres, cons, hir, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
-      for (let i = 0; i < cons.size(); i++) {
-        const c = cons.get(i);
-        const rect = cv.boundingRect(c);
-        cv.rectangle(image, new cv.Point(rect.x, rect.y), new cv.Point(rect.x + rect.width, rect.y + rect.height), new cv.Scalar(0, 0, 255))
-      }
-      cv.imshow(canvasDetectRef.current!, image);
+      cv.imshow(canvasDetectRef.current!, detected);
       setObjCount(cons.size())
     };
     task();
   }, [props.img, threshould]);
 
-
-
   return <div>
-    <Slider
-      value={threshould}
-      min={0}
-      max={255}
-      valueLabelDisplay='on'
-      onChange={(_, val) => setThreshould(val as number)}>
-    </Slider>
     <img width={500} height={500} ref={imageRef} src={props.img}></img>
     <Accordion>
       <AccordionSummary>
@@ -143,6 +165,14 @@ function CV(props: { img: string }) {
       <Typography>Detected [{objCount}]</Typography>
     </AccordionSummary>
     <canvas ref={canvasDetectRef}></canvas>
+
+    <Slider
+      value={threshould}
+      min={0}
+      max={255}
+      valueLabelDisplay='on'
+      onChange={(_, val) => setThreshould(val as number)}>
+    </Slider>
   </div >;
 
 }
